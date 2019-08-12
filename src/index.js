a94c0067940c7034718a2473001d865c144c07ef
Elad Ezra <xxeladxx@gmail.com>
2019-08-12 13:27:04 +0300

    nu ma?2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from '../../src/App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import { Item } from '../src/stores/Item'
 import { ShoppingList } from '../src/stores/ShoppingList'
 
-
+//sombreros
 let potatoes = new Item("Potatoes")
 let sombreros = new Item("Sombreros")
 let corn = new Item("Corn")
